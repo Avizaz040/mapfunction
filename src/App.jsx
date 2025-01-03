50cd9e7e4b094618545c151043b5788574e4aa51
Avishek Mondal <avizaz040@gmail.com>
2025-01-03 20:05:02 +0530

    App.css h1 update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Card from "./Components/Card";
 function App() {
   return (
     <div className="min-h-screen bg-zinc-100 mx-auto">
-      <h1 className="text-3xl font-bold text-center text-gray-800 pt-8">Your Feedback</h1>
+      <h1 className="text-3xl font-bold text-center text-gray-800 pt-8">User Feedback</h1>
       <Card />
     </div>
   );
