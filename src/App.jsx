2e5328d6211d54929486965ed0fa662fd95e1f14
Avishek Mondal <avizaz040@gmail.com>
2025-01-03 18:43:29 +0530

    Initial first comment

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./Components/Card";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-zinc-100 content-center">
+      <h1 className="text-3xl font-bold text-center text-gray-800 mt-8">Your Feedback</h1>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Card.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Data } from "../Data/DataList";
+
+function CardList({ data }) {
+  return (
+    <ul className="space-y-8">
+      {data.map((item, index) => (
+        <li key={index} className="text-sm leading-6">
+          <div className="bg-[#f7fafc] rounded-lg p-4 space-y-4 m-[4px]">
+            <div className="flex flex-row items-start space-x-4">
+              <img
+                src={item.Image}
+                alt="Image"
+                className="w-14 h-14 object-cover rounded-full"
+              />
+              <div className="text-left">
+                <h1 className="text-xl font-semibold text-gray-800">{item.title}</h1>
+                <p className="font-semibold text-gray-400">{item.Designation}</p>
+              </div>
+            </div>
+            <p className="text-black">{item.description}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function Card() {
+  const getUniqueData = (data, startIndex, endIndex) => {
+    return data.slice(startIndex, endIndex);
+  };
+
+  const firstUlData = getUniqueData(Data, 0, Math.ceil(Data.length / 3));
+  const secondUlData = getUniqueData(Data, Math.ceil(Data.length / 3), Math.ceil((2 * Data.length) / 3));
+  const thirdUlData = getUniqueData(Data, Math.ceil((2 * Data.length) / 3), Data.length);
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 text-2xl text-black px-[10rem] py-[3rem] gap-6 mx-auto ">
+      <CardList data={firstUlData} />
+      <CardList data={secondUlData} />
+      <CardList data={thirdUlData} />
+    </div>
+  );
+}
+
+export default Card;
